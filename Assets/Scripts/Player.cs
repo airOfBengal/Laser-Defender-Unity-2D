22b08e1df61e74a8f423f92f510cf90c54c3f707
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2021-02-05 16:14:35 +0600

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 0.5f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 20f;
+    [SerializeField] float projectileFiringSpeed = 0.05f;
+
+    Coroutine firingCoroutine;
+
+    float xMin, xMax, yMin, yMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetMovementBoundaries();
+    }
+
+    private void SetMovementBoundaries()
+    {
+        Camera camera = Camera.main;
+        xMin = camera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = camera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = camera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = camera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            firingCoroutine = StartCoroutine(FireContinuously());
+        }
+
+        if(Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(firingCoroutine);
+        }
+    }
+
+    IEnumerator FireContinuously()
+    {
+        while(true)
+        {
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            yield return new WaitForSeconds(projectileFiringSpeed);
+        }
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
