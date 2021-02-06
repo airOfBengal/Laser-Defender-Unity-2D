f4cc0edacf47fcd38adfe5a0b4d3cacd5fe7ca34
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2021-02-06 13:13:42 +0600

    Adds spawn multiple enemies

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] WaveConfig waveConfig;
+    List<Transform> wayPaths;
+    [SerializeField] float moveSpeed = 5f;
+
+    int wayPointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wayPaths = waveConfig.GetWaypoints();
+        transform.position = wayPaths[wayPointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if(wayPointIndex < wayPaths.Count)
+        {
+            var targetPosition = wayPaths[wayPointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position,
+                targetPosition, movementThisFrame);
+
+            if(transform.position == targetPosition)
+            {
+                wayPointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
